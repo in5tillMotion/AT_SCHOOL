6fc5786b5b890a7bccfdaa17a34ab5213b927b66
in5tillMotion <waifusonmymind@gmail.com>
2020-07-14 18:11:11 +0400

    Final_Version

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        CompareObjectsTest.testCompareObjects();
-        MapTest.testMap();
         SortTest.testSort();
 
     }
