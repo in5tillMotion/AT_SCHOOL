5fd7212a67fe5ae990184bd6172b1afcfd8d9c70
in5tillMotion <waifusonmymind@gmail.com>
2020-07-14 18:06:45 +0400

    Create SortTest and check Test

---
+++ src/test/java/SortTest.java
@@ -0,0 +1,16 @@
+public class SortTest {
+    public static void testSort() {
+        reverseSortTest();
+    }
+
+    private static void reverseSortTest() {
+        System.out.println("\nMethod Validation Class Sort");
+        int[] test1 = new int[]{Integer.MIN_VALUE, -9, -1, 0, 7, 10, 19, 42, 73, Integer.MAX_VALUE};
+        int[] arr = Sort.reverseSort(test1);
+        int[] revTest1 = new int[]{Integer.MAX_VALUE, 73, 42, 19, 10, 7, 0, -1, -9, Integer.MIN_VALUE};
+        Boolean result = test1.equals(revTest1);
+        if (result == false) System.out.println("Test failed");
+        else System.out.println("Test successful");
+    }
+
+}
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         CompareObjectsTest.testCompareObjects();
         MapTest.testMap();
+        SortTest.testSort();
 
     }
 }
